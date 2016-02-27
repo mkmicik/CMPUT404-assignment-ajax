918ba0bd71df9fedbdb86023defa2312d6944f69
mkmicik <mkmicik@ualberta.ca>
2016-02-27 14:57:12 -0700

    Finalized project. AJAX functionality and prettier drawing

--- server.py
+++ server.py
@@ -74,27 +74,36 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return flask.redirect("/static/index.html")
+    
+@app.route("/static/index.html")
+def index():
+    '''Return /static/index.html '''
+    return app.send_static_file('index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    v = flask_post_json()
+    myWorld.set( entity, v )
+    e = myWorld.get(entity) 
+    return json.dumps( e )
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.space)
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return world()
 
 if __name__ == "__main__":
     app.run()
